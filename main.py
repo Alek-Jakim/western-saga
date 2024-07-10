0120e6ba10045feaee23d6fb65cf46f1c57fd9d2
Alek <alek.jakimovski1@gmail.com>
2024-07-10 15:28:57 +0200

    player movement & animations

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pygame, sys
 from settings import *
+from scripts.player import Player
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Western Saga")
         self.clock = pygame.time.Clock()
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+
+        self.setup()
+
+    def setup(self):
+        self.player = Player((200, 200), self.all_sprites, PATHS["player"], None)
 
     def run(self):
         while True:
@@ -17,6 +26,12 @@ class Game:
                     pygame.quit()
                     sys.exit()
             dt = self.clock.tick() / 1000
+
+            self.all_sprites.update(dt)
+
+            self.screen.fill("black")
+
+            self.all_sprites.draw(self.screen)
 
             pygame.display.update()
 
---
+++ scripts/player.py
@@ -0,0 +1,118 @@
+import pygame
+from pygame.math import Vector2 as Vec2
+from settings import *
+from os import walk
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups, path, collision_sprites):
+        super().__init__(groups)
+
+        self.import_assets(path)
+
+        self.status = "down_idle"
+        self.frame_index = 0
+        self.frame_speed = 10
+
+        self.image = self.animations[self.status][self.frame_index]
+        self.rect = self.image.get_rect(center=pos)
+
+        # float based movement
+        self.pos = Vec2(self.rect.center)
+        self.dir = Vec2()
+
+        self.speed = 200
+
+        # collisions
+        self.hitbox = self.rect.inflate(0, -self.rect.height / 2)
+        self.collision_sprites = collision_sprites
+
+        self.is_attacking = False
+
+    def import_assets(self, path):
+        self.animations = {}
+        # print(root_path + path)
+        for index, folder in enumerate(walk(path)):
+            if index == 0:
+                for name in folder[1]:
+                    if name not in self.animations:
+                        self.animations[name] = []
+
+            else:
+                for file_name in sorted(
+                    folder[2], key=lambda string: string.split(".")[0]
+                ):
+                    file_path = folder[0].replace("\\", "/") + "/" + file_name
+                    surf = pygame.image.load(file_path).convert_alpha()
+
+                    key = folder[0].split("\\")[1]
+                    self.animations[key].append(surf)
+
+    def get_status(self):
+        # idle
+        if self.dir.x == 0 and self.dir.y == 0:
+            self.status = self.status.split("_")[0] + "_idle"
+
+        if self.is_attacking:
+            self.status = self.status.split("_")[0] + "_attack"
+
+    def animate(self, dt):
+        current_animation = self.animations[self.status]
+
+        self.frame_index += self.frame_speed * dt
+
+        if self.frame_index >= len(current_animation):
+            self.frame_index = 0
+            if self.is_attacking:
+                self.is_attacking = False
+
+        self.image = current_animation[int(self.frame_index)]
+
+    def keyboard_input(self):
+        keys = pygame.key.get_pressed()
+
+        if not self.is_attacking:
+            if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+                self.dir.x = -1
+                self.status = "left"
+            elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+                self.dir.x = 1
+                self.status = "right"
+            else:
+                self.dir.x = 0
+
+            if keys[pygame.K_w] or keys[pygame.K_UP]:
+                self.dir.y = -1
+                self.status = "up"
+            elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
+                self.dir.y = 1
+                self.status = "down"
+            else:
+                self.dir.y = 0
+
+        if keys[pygame.K_SPACE]:
+            self.is_attacking = True
+            self.dir = Vec2()
+            self.frame_index = 0
+
+    def move(self, dt):
+
+        # Normalize vector to maintain diagonal speed
+        if self.dir.magnitude() != 0:
+            self.dir = self.dir.normalize()
+
+        self.pos.x += self.dir.x * dt * self.speed
+        self.hitbox.centerx = round(self.pos.x)
+        self.rect.x = self.hitbox.centerx
+
+        self.pos.y += self.dir.y * dt * self.speed
+        self.hitbox.centery = round(self.pos.y)
+        self.rect.y = self.hitbox.centery
+
+    def update(self, dt):
+        self.keyboard_input()
+        self.get_status()
+        self.move(dt)
+        self.animate(dt)
+
+        print(self.status)
