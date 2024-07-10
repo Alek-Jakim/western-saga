b90af582e451deb97701c192c25d9e1a701118c5
Alek <alek.jakimovski1@gmail.com>
2024-07-10 15:44:22 +0200

    add camera

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 import pygame, sys
 from settings import *
 from scripts.player import Player
+from pygame.math import Vector2 as Vec2
+
+
+# Camera - replace vanilla draw method with a custom one so that camera follows player
+class AllSprites(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.offset = Vec2()
+        self.screen = pygame.display.get_surface()
+        self.background = pygame.image.load(
+            root_path + "/graphics/other/bg.png"
+        ).convert()
+
+    def custom_draw(self, player):
+
+        # Change offset
+        self.offset.x = player.rect.centerx - WINDOW_WIDTH / 2
+        self.offset.y = player.rect.centery - WINDOW_HEIGHT / 2
+
+        # Blit surf
+        self.screen.blit(self.background, -self.offset)
+
+        for sprite in self.sprites():
+            offset_rect = sprite.image.get_rect(center=sprite.rect.center)
+            offset_rect.center -= self.offset
+            self.screen.blit(
+                sprite.image,
+                offset_rect,
+            )
 
 
 class Game:
@@ -11,7 +40,7 @@ class Game:
         self.clock = pygame.time.Clock()
 
         # groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
 
         self.setup()
 
@@ -31,7 +60,7 @@ class Game:
 
             self.screen.fill("black")
 
-            self.all_sprites.draw(self.screen)
+            self.all_sprites.custom_draw(self.player)
 
             pygame.display.update()
 
